07bf3d190f65c8d4a83ba339c78f0b01f6b9549e
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-12-05 14:26:56 -0300

    implementado uma função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    to_lower_word = word.lower()
+    words_found = list()
+    evidence = list()
+    for item in instance.line:
+        repeated_word = 0
+        for text in item['linhas_do_arquivo']:
+            repeated_word += 1
+            if to_lower_word in text.lower():
+                evidence.append({"linha": repeated_word})
+        if len(evidence) == 0:
+            return evidence
+        res = {
+            "palavra": word.lower(),
+            "arquivo": item['nome_do_arquivo'],
+            "ocorrencias": evidence
+        }
+        words_found.append(res)
+    return words_found
 
 
 def search_by_word(word, instance):
